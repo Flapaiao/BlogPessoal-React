9b17d46ca105bb19441eab9d55f851dd6b12a885
Flavia R. Paião <flarpaiao.home@gmail.com>
2024-02-27 10:52:38 -0300

    Atualização Perfil

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,24 +1,38 @@
+import { useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../../contexts/AuthContext';
 
 function Navbar() {
+  let navigate = useNavigate()
+
+  const { usuario, handleLogout } = useContext(AuthContext)
+
+  function logout() {
+    handleLogout()
+    alert('Usuário deslogado com sucesso')
+    navigate('/login')
+  }
+
+  let navbarComponent
 
   return (
     <>
-     <div className='w-full bg-purple-700 text-white flex justify-center py-4'>
-          <div className="container flex justify-between text-lg">
-            <div className='text-2xl font-bold uppercase'>Blog Pessoal</div>
+      <div className='w-full bg-purple-600 text-white flex justify-center py-4'>
+        <div className="container flex justify-between text-lg">
+          <Link to='/home' className='text-2x1 font-bold uppercase'>Blog Pessoal</Link>
 
-            <div className='flex gap-4'>
-              <Link to='/login' className='hover:underline'>Login</Link>
-              <Link to='/home' className='hover:underline'>Home</Link>
-              <div className='hover:underline'>Postagens</div>
-              <div className='hover:underline'>Temas</div>
-              <div className='hover:underline'>Cadastrar tema</div>
-              <div className='hover:underline'>Perfil</div>
-              <div className='hover:underline'>Sair</div>
-            </div>
+          <div className='flex gap-4'>
+            <Link to='/home' className='hover:text-purple-900'>Home</Link>
+            <Link to='/postagens' className='hover:text-purple-900'>Postagens</Link>
+            <Link to='/temas' className='hover:text-purple-900'>Temas</Link>
+            <Link to='/cadastroTema' className='hover:text-purple-900'>Cadastrar tema</Link>
+            <Link to='/login' className='hover:text-purple-900'>Login</Link>
+            <Link to='/perfil' className='hover:text-purple-900'>Perfil</Link>
+            <Link to='' onClick={logout} className='hover:text-purple-900'>Sair</Link>
+            
           </div>
         </div>
+      </div>
     </>
   )
 }
